4cc607b7b16f0718207553174eabf9f39e688f33
Jose Wills <jose.wills3@gmail.com>
2023-05-20 22:55:43 +0100

    passed CRUD operations tests

--- src/controllers/reader.js
+++ src/controllers/reader.js
@@ -2,5 +2,46 @@ const { Reader } = require('../models');
 
 exports.addReader = async (req, res) => {
   const newReader = await Reader.create(req.body);
+
   res.status(201).json(newReader);
+};
+
+exports.getAllReaders = async (req, res) => {
+  const readers = await Reader.findAll();
+
+  res.status(200).json(readers);
+};
+
+exports.getReader = async (req, res) => {
+  const readerId = req.params.id;
+  const reader = await Reader.findByPk(readerId);
+
+  if (reader) {
+    res.status(200).json(reader);
+  } else {
+    res.status(404).json({ error: 'The reader could not be found.' });
+  }
+};
+
+exports.updateReader = async (req, res) => {
+  const readerId = req.params.id;
+  const updateData = req.body;
+  const [ updatedRows ] = await Reader.update(updateData, { where: { id: readerId } });
+
+  if (updatedRows) {
+    res.status(200).json(updatedRows);
+  } else {
+    res.status(404).json({ error: 'The reader could not be found.' });
+  }
+};
+
+exports.deleteReader = async (req, res) => {
+  const readerId = req.params.id;
+  const deletedRows = await Reader.destroy({ where: { id: readerId } });
+
+  if (deletedRows) {
+    res.status(204).json(deletedRows);
+  } else {
+    res.status(404).json({ error: 'The reader could not be found.' });
+  }
 };
--- src/routes/reader.js
+++ src/routes/reader.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { addReader } = require('../controllers/reader');
+const { addReader, getAllReaders, getReader, updateReader, deleteReader } = require('../controllers/reader');
 
 const readerRouter = express.Router();
 
-readerRouter.route('/').post(addReader);
+readerRouter.route('/').post(addReader).get(getAllReaders);
+
+readerRouter.route('/:id').get(getReader).patch(updateReader).delete(deleteReader);
 
 module.exports = readerRouter;
